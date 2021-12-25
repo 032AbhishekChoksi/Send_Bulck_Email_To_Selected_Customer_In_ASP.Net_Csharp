9597a37ca0d44d34ab9724468b11e2bcd4c2ed0c
032AbhishekChoksi <19bmiit032@gmail.com>
2021-12-25 22:32:21 +0530

    Add project files.

---
+++ SendBulkEmailToSelectedCustomer/index.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SendBulkEmailToSelectedCustomer
+{
+    public partial class index : System.Web.UI.Page
+    {
+        FunctionClass fun = new FunctionClass();
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+
+            if (!Page.IsPostBack)
+            {
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from CustomerList";
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                r1.DataSource = dt;
+                r1.DataBind();
+            }
+        }
+
+        protected void bttnsubmit_Click(object sender, EventArgs e)
+        {
+
+            string value = "";
+            foreach (RepeaterItem item in r1.Items)
+            {
+                var checkbox = item.FindControl("rbt_details") as CheckBox;
+                if (checkbox.Checked)
+                {
+                    //value += checkbox.Text + ",";
+                    value += fun.sendEmail(checkbox.Text, "Send Bulk Email to Selected Customers in Asp.Net C#", "Bulk Email Testing") + " , ";
+
+                }
+            }
+            lblMessage.Text = "<b>" + value + "</b>";
+
+        }
+    }
+}
